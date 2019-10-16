9d84ed7736636e8eeb78764f5adde3f68d443a88
orangespriteman <hervinsagnep@gmail.com>
2019-10-15 20:41:40 -0700

    Add files via upload

--- jsps2.js
+++ jsps2.js
@@ -18,44 +18,46 @@ function moneyCounting() {
 
 function softwareSales() {
     var numOrdered = parseInt(prompt("Enter the number of packages ordered."));
-    var packageCost = 99;
+    var packageCost = 100;
     var total = numOrdered * packageCost;
-    var finalPrice = total - (total * discount)
     if (numOrdered >= 10 && numOrdered <= 19) {
-        var discount = .10;
-        console.log("Discount: " + total * discount);
-        cosole.log("Total: " + finalPrice)
+        console.log("Your discount is " + (total * .10) + " dollars.");
+        console.log("The total amount after the discount is " + (total - (total * .10)) + " dollars.");
     }
     else if (numOrdered >= 20 && numOrdered <= 49) {
-        var discount = .20;
-        console.log("Discount: " + total * discount);
-        cosole.log("Total: " + finalPrice)
+        console.log("Your discount is " + (total * .20) + " dollars.");
+        console.log("The total amount after the discount is " + (total - (total * .20)) + " dollars.");
     }
     else if (numOrdered >= 50 && numOrdered <= 99) {
-        var discount = .30;
-        console.log("Discount: " + total * discount);
-        cosole.log("Total: " + finalPrice)
+        console.log("Your discount is " + (total * .30) + " dollars.");
+        console.log("The total amount after the discount is " + (total - (total * .30)) + " dollars.");
     }
     else if (numOrdered >= 100) {
-        var discount = .40;
-        console.log("Discount: " + total * discount);
-        cosole.log("Total: " + finalPrice)
+        console.log("Your discount is " + (total * .40) + " dollars.");
+        console.log("The total amount after the discount is " + (total - (total * .40))+ " dollars.");
     }
+	else{
+		console.log("Your discount is " + 0 + " dollars.");
+		console.log("The total amount after the discount is " + (total) + " dollars.");
+	}
 }
+
 function timeCalculator() {
     var secondInput = parseInt(prompt("Number of seconds"));
-    if (secondInput >= 60) {
-        console.log(secondInput / 60);
+    if (secondInput >= 60 && secondInput < 3600) {
+		var minutes = Math.trunc(secondInput/60);
+        console.log("Minutes: " + (minutes));
     }
-    else if (secondInput >= 3600) {
-        console.log(secondInput / 3600);
+    else if (secondInput >= 3600 && secondInput < 86400) {
+		var hours = Math.trunc(secondInput/3600);
+        console.log("Hours: " + (hours));
     }
     else if (secondInput >= 86400) {
-        console.log(secondInput / 86400);
-
+		var days = Math.trunc(secondInput/86400);
+        console.log("Days: " + (days));
     }
-    else {
-        console.log(secondInput);
+    else if (secondInput < 60) {
+        console.log("Seconds: " + secondInput);
     }
 }
 

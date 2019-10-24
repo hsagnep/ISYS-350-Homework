729a9e25086539f9d925e9df3ef3d1629e76ff29
orangespriteman <hervinsagnep@gmail.com>
2019-10-24 10:44:39 -0700

    Add files via upload

---
+++ jsps3.js
@@ -0,0 +1,47 @@
+function tuitionCount() {
+// Put your solution to Exercise #1 here.
+    var tuition = 8000;
+    for (i = 0; i < 5; i++) {
+        tuition += (tuition * .03);
+    }
+    console.log("The tuition amount will be " + Math.floor(tuition) + " dollars.");
+}
+
+
+function budgetAnalysis() {
+// Put your solution to Exercise #2 here.
+
+
+}
+
+
+function penniesFromHeaven() {
+// Put your solution to Exercise #3 here.
+    var days = 1;
+    var pennies = 1;
+    while (pennies <= 100000000) {
+        pennies += 2 ** days;
+        days += 1;
+    }
+    console.log("It takes " + (days+1) + " days to reach $1,000,000.");
+}
+
+
+
+function hiLo() {
+// Put your solution to Exercise #4 here
+    var randomNumber = Math.floor(Math.random() * 100) + 1;
+    while (true) {
+        var guess = parseInt(prompt("Enter Guess: "));
+        if (guess < randomNumber) {
+            console.log("Guess was too low");
+        }
+        else if (guess > randomNumber) {
+            console.log("Guess was too high");
+        }
+        else if (guess == randomNumber) {
+            console.log("You Won");
+            break;
+        }
+    }
+}

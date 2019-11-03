c04347c98143e9bf51a0edcaa7501056fe8be9dc
orangespriteman <hervinsagnep@gmail.com>
2019-11-03 00:27:49 -0700

    Add files via upload

---
+++ jsps4.js
@@ -0,0 +1,19 @@
+function propertyTax(value) {
+// Put your solution to Exercise #1 here.
+    var assesementValue = value * .6;
+    var propertyTax = assesementValue * .0064;
+    console.log(value);
+    //console.log("Property tax is " + propertyTax);
+}
+
+function budgetAnalysis() {
+// Put your solution to Exercise #2 here.
+}
+
+function penniesFromHeaven() {
+// Put your solution to Exercise #3 here.
+}
+
+function hiLo() {
+// Put your solution to Exercise #4 here.
+}
